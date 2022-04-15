cfcc17bc311d0acc559f6c07c4ca4b1a69f2e620
Seva <deaaalmail@gmail.com>
2022-04-15 15:22:37 +0300

    second commit

--- template/header.php
+++ template/header.php
@@ -1,3 +1,12 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>MyPortfolio</title>
+  </head>
+  <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <div class="container-fluid">
     <a class="navbar-brand" href="/index.php">Portfolio page</a>
@@ -13,10 +22,10 @@
           <a class="nav-link active" href="#">Results</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" href="/template/signinform.php">Sign in</a>
+          <a class="nav-link active" href="/signinform.php">Sign in</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" href="/template/register.php">Get Started</a>
+          <a class="nav-link active" href="/register.php">Get Started</a>
       </ul>
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
